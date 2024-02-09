85c9388f0b387e5930ac2d8c9dd882cb0082eb19
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-09 15:54:32 +0530

    adding marquee

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,13 +4,22 @@ import v1 from '/src/assets/v1.mp4'
 function Landing(){
     return (
         <div className="">
-        <video autoPlay loop muted className=" z-1">
+            
+
+           
+        <video autoPlay loop muted className="">
                 <source src={v1} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
            
+
+
+           </div>
+
+        
            
-        </div>
+           
+       
 
 
 
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,11 @@
+
+
+function Marquee(){
+    return(
+        <div className="rounded-t-3xl w-full h-48 bg-emerald-800">
+
+            
+        </div>
+    )
+}
+export default Marquee
