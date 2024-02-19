647f3a47cc057b548fdd3e1f04254b6531b8ba5a
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-20 02:29:53 +0530

    added the scroll down feature

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,20 @@
 
 import v1 from '/src/assets/v1.mp4'
 
+import img1 from'/src/assets/img1.jpg'
+import { VscGitFetch } from "react-icons/vsc";
+import { IoIosArrowDown } from "react-icons/io";
+
+
 function Landing(){
     return (
-        <div className=" w-full h-screen bg-zinc-900 pt-1">
+        <div className=" w-full h-screen bg-zinc-900 bg-[url(/src/assets/img1.jpg)] bg-cover pt-1">
            
 
            {/* <video src={v1} autoPlay muted loop></video> */}
        
 
-            <div className='z-[23] text mt-56 px-20'>
+            <div className='z-[23] text mt-56 px-20 '>
 
 
                 {["we create",,"personlized","learning expernices."].map((item,index)=>(
@@ -30,6 +35,13 @@ function Landing(){
                <div className='mt-10 border-zinc-600 border-2 rounded-md '>
                 <button className='pt-2 pb-2 pr-2 pl-2 capitalize'>Get register</button>
                </div>
+
+               
+             </div>
+             <div className='animate-bounce w-28 h-18 mt-8 mx-[723px]'>
+             <span className=' -mx-3 text-sm text-slate-400'>Scroll down</span>
+                <IoIosArrowDown className=' pl-1 w-10'></IoIosArrowDown>
+               
              </div>
          
                
