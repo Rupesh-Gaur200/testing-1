779e0ef9c2e37f0a0695c39d683099953cfe70e5
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-19 14:51:17 +0530

    Given the padding to top , to centerlize the h1 element

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,12 +3,14 @@
 function Marquee(){
     return(
        
-        <div className="rounded-t-3xl w-full h-48 bg-emerald-800 flex gap-8">
+        <div className="rounded-t-2xl w-full h-48 bg-emerald-800 ">
                
+               <div className="flex gap-8 mx-4">
               {["Ochi's let's begin","Ochi's let's begin"].map((item,index)=>(
-                <h1 key={index} className="pt-8 text-8xl">{item} </h1>
+            
+                <h1 key={index} className="pt-10 text-8xl">{item} </h1>
               )) }
-              
+              </div>
       
         </div>
     )
