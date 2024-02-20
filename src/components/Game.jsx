84c6a12af8b1181171bf58d7055c5c8cbc032b6e
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-21 01:44:59 +0530

    Created the Eye ball mouse tracking functionality

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,30 @@
 
+
+import { useEffect, useState } from "react"
+
+
 function Game(){
+
+    const [rotation , setRotation]=useState(0)
+
+useEffect (()=>{
+window.addEventListener("mousemove",(e)=>{
+  let x=e.clientX
+  let y=e.clientY
+ 
+  
+  let posx = x-window.innerWidth/2 
+  let posy =y-window.innerHeight/2 
+
+  var angle=Math.atan2(posy,posx)*(180/Math.PI)
+  console.log(angle)
+  setRotation(angle-180)
+
+})
+
+})
+
+
     return(
         
         <div className="w-full h-screen overflow-hidden">
@@ -9,16 +34,30 @@ function Game(){
 
                       <div className=" flex justify-center gap-10 absolute top-1/2  left-1/2 -translate-x-[50%] -translate-y-[80%]  ">
                         <div className=" flex items-center justify-center w-[12vw] h-[12vw] rounded-full bg-zinc-200 ">
-                        <div className="w-[8vw] h-[8vw] rounded-full bg-black ">
+                        <div className=" relative w-[8vw] h-[8vw] rounded-full bg-black ">
+
+
+                          <div style={{transform: `translate(-50%,-50%) rotate(${rotation}deg)`}} className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10 ">
+
+                          
+                        <div className="w-[2vw] h-[2vw] rounded-full bg-zinc-200 ">
                               
 
+                              </div>
+                              </div>
                               </div>
 
                         </div>
                         <div className=" flex items-center justify-center w-[12vw] h-[12vw] rounded-full bg-zinc-200 ">
-                        <div className="w-[8vw] h-[8vw] rounded-full bg-black ">
+                        <div className=" relative flex justify-center items-center w-[8vw] h-[8vw] rounded-full bg-black ">
+
+
+                          <div style={{transform: `translate(-50%,-50%) rotate(${rotation}deg)`}} className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]   w-full h-10">
+                        <div className="w-[2vw] h-[2vw] rounded-full bg-zinc-200 ">
                               
 
+                              </div>
+                              </div>
                               </div>
                         </div>
                       </div>
