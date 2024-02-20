41c60b20eb46f822f03ec90572871b0dc46b8b4a
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-20 23:34:29 +0530

    created the eyes

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,9 +4,33 @@ function Game(){
         
         <div className="w-full h-screen overflow-hidden">
         
-               <div className="w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]"></div>
+               <div className=" relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
+
+
+                      <div className=" flex justify-center gap-10 absolute top-1/2  left-1/2 -translate-x-[50%] -translate-y-[80%]  ">
+                        <div className=" flex items-center justify-center w-[12vw] h-[12vw] rounded-full bg-zinc-200 ">
+                        <div className="w-[8vw] h-[8vw] rounded-full bg-black ">
+                              
+
+                              </div>
+
+                        </div>
+                        <div className=" flex items-center justify-center w-[12vw] h-[12vw] rounded-full bg-zinc-200 ">
+                        <div className="w-[8vw] h-[8vw] rounded-full bg-black ">
+                              
+
+                              </div>
+                        </div>
+                      </div>
+
+
+
+
+               </div>
 
         </div>
+
+
 
           
     )
