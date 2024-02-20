c724a380645c8bd3abe0fd28a1246b92805a1c3c
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-20 14:54:30 +0530

    created the two componets of newsegment

--- src/components/NewSegment.jsx
+++ src/components/NewSegment.jsx
@@ -4,7 +4,64 @@ function NewSegment(){
  
 
     return(
-        <div className="bg-yellow-400 w-full h-screen">
+        <div className="bg-yellow-400 w-full py-20 rounded-t-3xl text-black">
+             
+              <div className=" px-20 w-[85%]">
+              <h1 className=" font-['Ubuntu'] leading-none   text-[40px]">Ochi is a strategic partner for fast-grow­ing edu learning that need to raise self proclaimed, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
+              </div>
+
+
+              <div className="border-t-2 border-b-2 border-zinc-500 mt-16 flex justify-between pb-24 pt-10 ">
+
+                <h1 className="px-20 w-[45%]">What you can expect:</h1>
+
+                <div className="flex flex-col gap-14  ">
+
+                    <div className=" ">
+
+                    <p className="w-60">
+We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it’s live or digital, delivered for one or a hundred people.</p>
+</div>
+
+
+
+                <div className="">
+                    <p className="w-60">
+                    We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.</p>
+</div>
+                    
+
+
+
+
+
+
+
+
+                </div>
+                   
+                
+                <div className="mx-40 pt-32">
+                    <h1>S:</h1>
+                    <ul className="hover:cursor-pointer ">
+                        <li className="hover:border-b-2 border-zinc-700">Instagram</li>
+                        <li className="hover:border-b-2 border-zinc-700">behance</li>
+                        <li className="hover:border-b-2 border-zinc-700">Twitter</li>
+                        <li className="hover:border-b-2 border-zinc-700">LinkedIn</li>
+                    </ul>
+                </div>
+
+               
+
+                
+
+                  
+
+              </div>
+                  
+
+              <div className=" mt-16"></div>     
+
 
         </div>
     )
