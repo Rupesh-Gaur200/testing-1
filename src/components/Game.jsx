ba42a4afe155de74f5a91d16b9c902b989fb7d9b
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-20 23:08:33 +0530

    Created the next game component

---
+++ src/components/Game.jsx
@@ -0,0 +1,15 @@
+
+function Game(){
+    return(
+        
+        <div className="w-full h-screen overflow-hidden">
+        
+               <div className="w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]"></div>
+
+        </div>
+
+          
+    )
+}
+
+export default Game
