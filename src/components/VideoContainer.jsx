6fb0c93cfef4266e1875e506f123e430749e5836
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-23 14:03:28 +0530

    Created the component for displaying courses video

---
+++ src/components/VideoContainer.jsx
@@ -0,0 +1,15 @@
+
+function VideoContainer(){
+    return(
+        <div className="w-full px-20 h-full border-2 border-red overflow-hidden">
+
+        <div className="w-full px-20 h-full border-2 border-green-200 "></div>
+
+         
+
+
+        </div>
+    )
+}
+
+export default VideoContainer
