47c4e6ea1a0b3ec2ae5895a532c3453cc7cb95ca
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-24 01:05:24 +0530

    displaying the fetch data in our site

--- src/components/VideoShow.jsx
+++ src/components/VideoShow.jsx
@@ -1,15 +1,15 @@
 
 
-function VideoShow(){
+function VideoShow({d}){
 
 
 
 
     return (
         <div className=" px-8 w-full h-full grid grid-cols-4  gap-4">
-            {["/src/assets/image2.jpg","/src/assets/image1.jpg","/src/assets/image3.jpg","/src/assets/image1.jpg","/src/assets/image2.jpg","/src/assets/image3.jpg","/src/assets/image2.jpg","/src/assets/image1.jpg","/src/assets/image3.jpg"].map((item ,index)=>(
+            {d.map((items ,index)=>(
                  <div key={index} className={`w-[350px]  h-[250px] py-10 px-10  `}  >
-                    <img className="w-full h-full" src={item}></img>
+                    <img className="w-full h-full" src={items.snippet.thumbnails.high.url}></img>
                  </div>
             ))}
  
