52f9477d4f8ac40f33c27c4963c1a079f28ea441
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-23 14:58:49 +0530

    Created video Show COmponet to showcase video using grid

---
+++ src/components/VideoShow.jsx
@@ -0,0 +1,21 @@
+
+
+
+function VideoShow(){
+    return (
+        <div className=" py-10 px-10 w-full h-full grid grid-cols-3  gap-4">
+        <div className=" w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="  w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="    w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="   w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="  w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="   w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400 "></div>
+   <div className="   w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400"></div>
+   <div className=" w-[350px]  h-[250px] border-2 border-red py-10 px-10 bg-green-400"></div>
+
+        </div>
+    )
+
+}
+
+export default VideoShow
