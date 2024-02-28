2aac76033dc02c4cc562370080c6d18fa718f46f
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-28 12:21:33 +0530

    configuring the title and descritption and altered the css of card div

--- src/components/VideoShow.jsx
+++ src/components/VideoShow.jsx
@@ -1,19 +1,22 @@
 
 
-function VideoShow({d}){
+function VideoShow({ d }) {
 
 
 
 
     return (
-        <div className=" px-8 w-full h-full grid grid-cols-4  gap-4">
-            {d.map((items ,index)=>(
-                 <div key={index} className={`w-[350px]  h-[250px] py-10 px-10  `}  >
-                    <img className="w-full h-full" src={items.snippet.thumbnails.high.url}></img>
-                 </div>
+        <div className=" px-8 w-full h-full grid grid-cols-4 gap-12 ">
+            {d.map((items, index) => (
+               <div key={index} className={`rounded-lg shadow-lg  w-[250px] h-[350px] mt-10 mb-10 bg-zinc-800 hover:scale-110 delay-75 hover:cursor-pointer `}>
+                  <img  className="rounded" src={items.snippet.thumbnails.high.url}></img>
+                   <p className="text-wrap text-semibold pl-3 pt-3 pr-3 overflow-hidden">{items.snippet.description}</p> 
+             </div>
+             
+             
             ))}
- 
- 
+
+
         </div>
     )
 
