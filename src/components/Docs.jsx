17bd596c7287c740e781daba2a97fbf33355d60b
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-29 15:21:23 +0530

    given the scrolling speed of .1 faster and child div to 0.2 faster

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -2,8 +2,8 @@
 
 function Docs(){
     return (
-        <div className="w-full h-screen overflow-hidden bg-slate-300 ">
-            <div  className="mx-20 my-48 flex justify-center items-center gap-12 ">
+        <div data-scroll data-scroll-speed='.1' className="w-full h-full overflow-hidden bg-slate-200 ">
+            <div data-scroll data-scroll-speed='0.2'  className="mx-20 my-48 flex justify-center items-center gap-12 ">
                 <div className="w-[45%] bg-cyan-400 h-[350px] rounded-xl "></div>
                 <div className="w-[25%] bg-emerald-400 h-[275px] rounded-xl "></div>
                 <div className="w-[25%] bg-purple-400 h-[200px] rounded-xl "></div>
