679b4a5c7ffa51d079a47b2a50dcf5f0e1fa8639
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-29 15:50:43 +0530

    taken to div make the in center and wanted orientation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,24 @@
 function Footer (){
     return (
         <div data-scroll data-scroll-speed='-.1'  className="w-full h-screen bg-green-300">
+            
+            <div className="pt-28  w-full h-full border-purple-700 border-2 flex overflow-hidden">
+                <div className=" mx-20 my-20 w-[50%] h-[85%] border-2 border-red-400">
+
+                    <h1 className="text-black text-6xl font-[Ubuntu]">Footer</h1>
+
+                </div>
+                <div className=" mx-20 my-20 w-[50%] h-[85%] border-2 border-pink-800">
+
+                                </div>
+            </div>
+            
+            
+            
+            
+            
+            
+            
              </div>
     )
 }
