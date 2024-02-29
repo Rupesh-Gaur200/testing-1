0a3f78b6f0dbdc7f4848e05cff5ec6617415d07d
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-29 15:51:31 +0530

    Changed the background color to bg-zinc-200

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -2,7 +2,7 @@
 
 function Docs(){
     return (
-        <div data-scroll data-scroll-speed='.1' className="w-full h-full overflow-hidden bg-slate-200 ">
+        <div data-scroll data-scroll-speed='.1' className="w-full h-full overflow-hidden bg-zinc-200">
             <div data-scroll data-scroll-speed='0.2'  className="mx-20 my-48 flex justify-center items-center gap-12 ">
                 <div className="w-[45%] bg-cyan-400 h-[350px] rounded-xl "></div>
                 <div className="w-[25%] bg-emerald-400 h-[275px] rounded-xl "></div>
