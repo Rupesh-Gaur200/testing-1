8a012109adf631fbc2b664b720703fe9273cb877
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2024-02-29 15:19:53 +0530

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Footer (){
+    return (
+        <div data-scroll data-scroll-speed='-.1'  className="w-full h-screen bg-green-300">
+             </div>
+    )
+}
+
+export default Footer 
